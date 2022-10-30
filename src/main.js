977ad99ab72cc287500f4c3a259b3ea71fe00d89
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-30 03:02:22 -0300

    add routes to main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import express from 'express';
-import morgan from 'morgan';
-import sequelize from './db.js';
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send('Hello World');
     }
 );
-
+app.use('/projects', require('./routes/projects'));
+app.use('/certifications', require('./routes/certifications'));
+app.use('/home', require('./routes/home'));
 app.listen(3000, () => {
     console.log('Server on port 3000');
     try{
-        sequelize.authenticate();
-        console.log('Database connected');
+        // sequelize.authenticate();
     }catch(e){
         console.log(e);
     }
