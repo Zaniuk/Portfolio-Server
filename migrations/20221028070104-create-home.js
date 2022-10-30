d9de2dc9b22fcd22ebbce83e2c926a62d7a73080
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-30 04:03:16 -0300

    fix home model and migration

--- migrations/20221028070104-create-home.js
+++ migrations/20221028070104-create-home.js
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Homes');
+    await queryInterface.dropTable('Home');
   }
 };
--- models/home.js
+++ models/home.js
@@ -20,6 +20,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Home',
+    freezeTableName: true
+
   });
   return Home;
 };
