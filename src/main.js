05bd016a08cbb8e0c9e597d1d00b2be103bab73b
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-30 15:19:48 -0300

    add cors

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-
+const cors = require('cors');
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
