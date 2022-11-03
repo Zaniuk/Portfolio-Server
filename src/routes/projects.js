8d7ed8aa5d2944301de679295eaf1f6839fcd7ce
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-02 23:21:23 -0300

    add project details

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,11 +1,12 @@
 // create route
 
 const express = require('express');
-const { getProjects, createProject, updateProject, deleteProject } = require('../controllers/projects');
+const { getProjects, createProject, updateProject, deleteProject, getProjectDetail } = require('../controllers/projects');
 const { verifyToken } = require('../middlewares/auth');
 const router = express.Router();
 
 router.get('/', getProjects);
+router.get('/:id', getProjectDetail);
 router.post('/', verifyToken , createProject);
 router.put('/:id', verifyToken , updateProject);
 router.delete('/:id', verifyToken , deleteProject);
